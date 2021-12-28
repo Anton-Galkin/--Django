12be4c29c37b1f2930bdf6bdb83b10315b96e6d0
Anton <4qguns@gmail.com>
2021-12-28 13:58:24 +0300

    task_8 Class Based Views (CBV), before Шаблонные фильтры

--- g_shop/adminapp/views.py
+++ g_shop/adminapp/views.py
@@ -8,19 +8,34 @@ from mainapp.models import Product, Category
 from django.contrib.auth.decorators import user_passes_test
 from adminapp.forms import ProductEditForm
 
+from django.views.generic.list import ListView
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 
-@user_passes_test(lambda u: u.is_superuser)
-def users(request):
-    title = 'Админка/пользователи'
 
-    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+class UsersListView(ListView):
+    model = ShopUser
+    template_name = 'adminapp/users.html'
 
-    content = {
-        'title': title,
-        'objects': users_list
-    }
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-    return render(request, 'adminapp/users.html', content)
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def users(request):
+#     title = 'Админка/пользователи'
+#
+#     users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+#
+#     content = {
+#         'title': title,
+#         'objects': users_list
+#     }
+#
+#     return render(request, 'adminapp/users.html', content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -44,7 +59,7 @@ class ShopUserAdminEditForm:
     pass
 
 
-@user_passes_test(lambda u: u.is_superuser)
+# @user_passes_test(lambda u: u.is_superuser)
 def user_update(request, pk):
     title = 'пользователи/редактирование'
 
@@ -96,17 +111,49 @@ def categories(request):
     return render(request, 'adminapp/categories.html', content)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def category_create(request):
-    pass
+class ProductCategoryCreateView(CreateView):
+    model = Category
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    fields = '__all__'
 
 
-def category_update(request, pk):
-    pass
+# def category_create(request):
+#     pass
 
 
-def category_delete(request, pk):
-    pass
+class ProductCategoryUpdateView(UpdateView):
+    model = Category
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+
+        return context
+
+
+# def category_update(request, pk):
+#   pass
+
+
+class ProductCategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'adminapp/category_delete.html'
+    success_url = reverse_lazy('admin:categories')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+
+# def category_delete(request, pk):
+#     pass
 
 
 def products(request, pk):
@@ -143,12 +190,17 @@ def product_create(request, pk):
         return render(request, 'adminapp/product_update.html', content)
 
 
-def product_read(request, pk):
-    title = 'продукт/подробнее'
-    product = get_object_or_404(Product, pk=pk)
-    content = {'title': title, 'object': product, }
+# def product_read(request, pk):
+#     title = 'продукт/подробнее'
+#     product = get_object_or_404(Product, pk=pk)
+#     content = {'title': title, 'object': product, }
+#
+#     return render(request, 'adminapp/product_read.html', content)
 
-    return render(request, 'adminapp/product_read.html', content)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'adminapp/product_read.html'
 
 
 def product_update(request, pk):
